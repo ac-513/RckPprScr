29514235cee681869fa2ed5810cf4c224beb3b91
Aroub <173303106+ac-513@users.noreply.github.com>
2024-08-08 20:06:02 -0400

    everyhting is amost there, just need to debug the else statement being captured in game function

--- jscript.js
+++ jscript.js
@@ -1,3 +1,5 @@
+
+
 function computerChoice() {
   let cChoice = "";
 
@@ -16,95 +18,114 @@ function computerChoice() {
   return cChoice;
 }
 
-let rckFunc = () => {
-  document.querySelector(".outcomeSign").style.display = "flex";
-  humanChoice = "Rock";
-  return humanChoice;
-};
-let pprFunc = () => {
-  document.querySelector(".outcomeSign").style.display = "flex";
-  humanChoice = "Paper";
-  return humanChoice;
-};
-let scrsFunc = () => {
-  document.querySelector(".outcomeSign").style.display = "flex";
-  humanChoice = "Scissors";
-  return humanChoice;
-};
-finalArr = [];
-let playRound = (computerChoice) => {
-  let humanChoice = "";
-  var winner = "";
-  let compScore = 0;
-  let humanScore = 0;
 
-  document.querySelector("#rockBtn").addEventListener("click", rckFunc);
-  document.querySelector("#paperBtn").addEventListener("click", pprFunc);
-  document.querySelector("#scissorBtn").addEventListener("click", scrsFunc);
+
+
+
+// let humanPick = () => {
+//   let humanChoice = "";
+//   let rckFunc = () => {
+//     // document.querySelector(".outcomeSign").style.display = "flex";
+//     humanChoice = "Rock";
+//     console.log("chosen:", humanChoice);
+//   };
+//   let pprFunc = () => {
+//     // document.querySelector(".outcomeSign").style.display = "flex";
+//     humanChoice = "Paper";
+//     // console.log('chosen:', humanChoice)
+//   };
+//   let scrsFunc = () => {
+//     // document.querySelector(".outcomeSign").style.display = "flex";
+//     humanChoice = "Scissors";
+//     console.log("chosen:", humanChoice);
+//   };
+
+
+//   return humanChoice;
+// };
+
+
+let winner = "";
+let compScore = 0;
+let humanScore = 0;
+
+
+let playRound = (humanChoice) => {
+  computerChoice();
+
+  console.log(computerChoice());
+  console.log(humanChoice);
 
   if (humanChoice == "Rock" && computerChoice == "Rock") {
     winner = "Tie, both recieve points!";
     compScore += 1;
     humanScore += 1;
-  }
-  else if (humanChoice == "Rock" && computerChoice == "Paper") {
+  } else if (humanChoice == "Rock" && computerChoice == "Paper") {
     winner = "Computer wins --> paper covers rock";
     compScore += 1;
-  }
-  else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+  } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
     winner = "You win --> rock beats scissors ";
     humanScore += 1;
-  }
-  else if (humanChoice == "Paper" && computerChoice == "Paper") {
+  } else if (humanChoice == "Paper" && computerChoice == "Paper") {
     winner = "Tie, both recieve points!";
     compScore += 1;
     humanScore += 1;
-  }
-  else if (humanChoice == "Paper" && computerChoice == "Rock") {
+  } else if (humanChoice == "Paper" && computerChoice == "Rock") {
     winner = "You win --> paper covers rock";
     humanScore += 1;
-  }
-  else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+  } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
     winner = "Computer wins --> scissors cuts paper ";
     compScore += 1;
-  }
-  else if (humanChoice == "Scissors" && computerChoice == "Scissors") {
+  } else if (humanChoice == "Scissors" && computerChoice == "Scissors") {
     winner = "Tie, both recieve points!";
     compScore += 1;
     humanScore += 1;
-  }
-  else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+  } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
     winner = "You win --> scissors cuts paper";
     humanScore += 1;
-  }
-  else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+  } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
     winner = "Computer wins --> Rock ";
     compScore += 1;
+  } else {
+    winner = "error";
+    humanScore = -1;
+    compScore = -1;
   }
-  else{
-    winner='error';
-    humanScore=-1;
-    compScore=-1;
-  }
-  finalArr[0] = winner;
-  finalArr[1] = humanScore;
-  finalArr[2] = compScore;
-  console.log(finalArr);
-
-  return finalArr;
+  // console.log(winner)
+  display();
 };
-document.getElementById("txt").innerHTML = finalArr[0];
-document.querySelector(".humanCounter").innerHTML = finalArr[1];
-document.querySelector(".computerCounter").innerHTML = finalArr[2];
 
-playRound(computerChoice());
 
 let restart = () => {
   document.getElementById("restartBtn").addEventListener("click", function () {
     document.querySelector(".outcomeSign").style.display = "none";
     document.querySelector(".humanCounter").innerHTML = "0";
     document.querySelector(".computerCounter").innerHTML = "0";
+    computerChoice();
   });
 };
 
+const display = () => {
+  document.querySelector(".outcomeSign").style.display = "flex";
+  document.getElementById("txt").innerHTML = winner;
+  document.querySelector(".humanCounter").innerHTML = humanScore;
+  document.querySelector(".computerCounter").innerHTML = compScore;
+};
+
+// playRound();
+// playRound(humanPick(), computerChoice());
+
+// humanPick();
+// playgame();
+
 restart();
+
+
+const rock=document.querySelector("#rockBtn");
+const paper= document.querySelector("#paperBtn");
+const scissors=document.querySelector("#scissorBtn");
+
+
+rock.addEventListener("click", function(){playRound('Rock')});
+paper.addEventListener("click", function(){playRound('Paper')});
+scissors.addEventListener("click",function(){playRound('Scissors')});
